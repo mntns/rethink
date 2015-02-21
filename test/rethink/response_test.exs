0e6870f7319d6952cfb52a295c5f5c85c8367517
Eddy Shure <mono@tucana.uberspace.de>
2015-02-22 00:25:28 +0100

    Fixed this little test and added supervisor.

--- test/rethink/response_test.exs
+++ test/rethink/response_test.exs
@@ -2,6 +2,6 @@ defmodule Rethink.Response.Test do
   use ExUnit.Case
 
   test "gives back actual resp" do
-    assert Rethink.Response.process('{"t":1,"r":[7]}') == [7]
+    assert Rethink.Response.process("{\"t\":1,\"r\":[7]}") == [7]
   end
 end
