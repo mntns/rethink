c68b708cffca4fd67b55a1cda890857c8094d26a
Eddy Shure <mono@tucana.uberspace.de>
2015-02-21 22:29:12 +0100

    Added the bare minimum.

---
+++ mix.exs
@@ -0,0 +1,20 @@
+defmodule Rethink.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :rethink,
+     version: "0.0.1",
+     elixir: "~> 1.0",
+     deps: deps]
+  end
+
+  def application do
+    [applications: [:logger],
+     mod: {Rethink, []}]
+  end
+
+  defp deps do
+    [{:exjsx,  github: "talentdeficit/exjsx"},
+     {:socket, github: "meh/elixir-socket"}]
+  end
+end
---
+++ test/rethink_test.exs
@@ -0,0 +1,7 @@
+defmodule RethinkTest do
+  use ExUnit.Case
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
